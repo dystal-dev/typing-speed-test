a6d138ab78ab3be4e62a757016634878eed27aab
dystal-dev <jeremyhabal@yahoo.com>
2026-01-20 21:56:05 +0800

    feat: added typography design tokens

--- src/components/TextContainer.jsx
+++ src/components/TextContainer.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function TextContainer() {
   return (
     <div>
-      <div>Start Typing Test Or click the text and start typing</div>
-      <input className="w-full"></input>
+      <div className="text-preset-3">
+        Start Typing Test Or click the text and start typing
+      </div>
+      <input className="w-full text-preset-1-regular"></input>
       <div>
         Test Complete! Solid run. Keep pushing to beat your high score. WPM:
         Accuracy: Characters: Go Again
