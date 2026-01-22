c1f4003900d852879b36f99e0e03966e1806a199
dystal-dev <jeremyhabal@yahoo.com>
2026-01-23 00:14:26 +0800

    refactor: converted generateRandomNumber into an arrow function

--- src/utils/passages.js
+++ src/utils/passages.js
@@ -1,12 +1,12 @@
-import passages from "../data/data.json" with { type: "json" };
+import data from "../data/data.json" with { type: "json" };
 
-function generateRandomNumber(arg) {
-  const randomNum = Math.floor(Math.random() * arg);
-  return randomNum;
-}
+export default function getRandomPassage(difficulty) {
+  const generateRandomNumber = (arg) => {
+    const randomNum = Math.floor(Math.random() * arg);
+    return randomNum;
+  };
 
-export function getRandomPassage(difficulty) {
-  const passage = passages[difficulty];
+  const passage = data[difficulty];
   const selectedPassage = passage[generateRandomNumber(passage.length)];
   return selectedPassage;
 }
