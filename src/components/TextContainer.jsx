b3535fab696722a747c4a1b505074462b4685272
dystal-dev <jeremyhabal@yahoo.com>
2026-01-23 00:15:48 +0800

    refactor: remove placeholder divs from TextContainer

--- src/components/TextContainer.jsx
+++ src/components/TextContainer.jsx
@@ -4,14 +4,7 @@ import TextBox from "./TextBox";
 export default function TextContainer() {
   return (
     <div>
-      <div className="text-preset-3">
-        Start Typing Test Or click the text and start typing
-      </div>
       <TextBox />
-      <div>
-        Test Complete! Solid run. Keep pushing to beat your high score. WPM:
-        Accuracy: Characters: Go Again
-      </div>
     </div>
   );
 }
