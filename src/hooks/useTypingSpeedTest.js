0af570da74f87ac6d1ac3e9ff60027162946ae5d
dystal-dev <jeremyhabal@yahoo.com>
2026-01-23 11:53:49 +0800

    refactor: moved typing speed test logic from component to hook

--- src/hooks/useTypingSpeedTest.js
+++ src/hooks/useTypingSpeedTest.js
@@ -1,4 +1,31 @@
-export function useTypingTest() {
-  //   const [typedText, setTypedText] = useState("");
-  //   const [isStarted, setIsStarted] = useState(false);
+import { useState } from "react";
+
+export function useTypingTest(passage) {
+  const [userInput, setUserInput] = useState("");
+  const [passageCharArray, setPassageCharArray] = useState(
+    passage.split("").map((char, index) => ({
+      char,
+      index,
+      isCorrect: null,
+    })),
+  );
+
+  function handleUserInputChange(event) {
+    const newInputvalue = event.target.value;
+
+    setUserInput(newInputvalue);
+    checkTypingAccuracy(newInputvalue);
+  }
+
+  function checkTypingAccuracy(inputValue) {
+    const inputArray = inputValue.split("");
+    const updatedArray = passageCharArray.map((item, i) => ({
+      ...item,
+      isCorrect: i < inputArray.length ? inputArray[i] === item.char : null,
+    }));
+
+    setPassageCharArray(updatedArray);
+  }
+
+  return { userInput, passageCharArray, handleUserInputChange };
 }
