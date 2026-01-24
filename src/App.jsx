4032ecb09d679e65a75053869e4341e16b30ddee
dystal-dev <jeremyhabal@yahoo.com>
2026-01-24 23:05:14 +0800

    feat(ui): added app max width

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import MainContent from "./components/MainContent";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+const maxAppWidthPX = 1440;
+
 function App() {
   return (
-    <div className="grid font-sora mx-1400 my-400 gap-800">
+    <div className="grid font-sora px-1400 py-400 gap-800 max-w-(--app-max-width) mx-auto">
       <Header />
       <MainContent />
       <Footer />
