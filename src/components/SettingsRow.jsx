782a3256ab7ee7e00ebb211d399857eb1b557350
dystal-dev <jeremyhabal@yahoo.com>
2026-01-26 16:55:11 +0800

    refactor: moved setting ui to its own component

--- src/components/SettingsRow.jsx
+++ src/components/SettingsRow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Setting from "./ui/Setting";
 import { settingsList } from "../data/settings";
 
 export default function SettingsRow() {
@@ -6,19 +6,12 @@ export default function SettingsRow() {
     <>
       <div className="flex divide-x divide-neutral-700">
         {settingsList.map((setting) => (
-          <ul
+          <Setting
             key={setting.label}
-            className="text-preset-5 text-neutral-400 gap-75 flex items-center px-200 last:pr-0 first:pl-0"
-          >
-            <span className="pr-75">{setting.label}:</span>
-            {setting.options.map((option) => (
-              <li key={option}>
-                <button className="btn cursor-pointer text-preset-5 text-neutral-0 px-125 py-75 rounded-8 border-1 border-neutral-500">
-                  {option}
-                </button>
-              </li>
-            ))}
-          </ul>
+            label={setting.label}
+            options={setting.options}
+            isActive={setting.isActive}
+          />
         ))}
       </div>
     </>
--- src/components/ui/Setting.jsx
+++ src/components/ui/Setting.jsx
@@ -1,3 +1,17 @@
-export default function Setting() {
-  return <div>Setting</div>;
+export default function Settings({ label, options, isActive }) {
+  return (
+    <ul
+      key={label}
+      className="text-preset-5 text-neutral-400 gap-75 flex items-center px-200 last:pr-0 first:pl-0"
+    >
+      <span className="pr-75">{label}:</span>
+      {options.map((option) => (
+        <li key={option}>
+          <button className="btn cursor-pointer text-preset-5 text-neutral-0 px-125 py-75 rounded-8 border-1 border-neutral-500">
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
