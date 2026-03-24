b730f2b0205f13b77575a538cf178402f1217b10
dystal-dev <jeremyhabal@yahoo.com>
2026-03-24 21:10:03 +0800

    refactor: changed settings format to work with updated settings logic

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,8 +1,37 @@
 export const settingsList = [
-  { id: "difficulty", options: ["easy", "medium", "hard"], default: "easy" },
+  {
+    id: "difficulty",
+    options: [
+      {
+        id: "easy",
+        label: "Easy",
+      },
+      {
+        id: "medium",
+        label: "Medium",
+      },
+      {
+        id: "hard",
+        label: "Hard",
+      },
+    ],
+    default: 0,
+  },
   {
     id: "mode",
-    options: ["timed (60s)", "passage"],
-    default: "timed (60s)",
+    options: [
+      {
+        id: "timed_60",
+        label: "Timed (60s)",
+        type: "timed",
+        startTime: 10,
+      },
+      {
+        id: "passage",
+        label: "Passage",
+        type: "passage",
+      },
+    ],
+    default: 0,
   },
 ];
