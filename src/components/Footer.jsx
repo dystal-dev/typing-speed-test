03457c45c32854329bb7608bbc2e7f3422300fee
dystal-dev <jeremyhabal@yahoo.com>
2026-03-26 18:33:04 +0800

    feat: added footer border

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,19 @@ import IconRestart from "../assets/images/icon-restart.svg";
 
 export default function Footer({ status, onRestart }) {
   return (
-    <div className="mx-auto">
+    <>
       {status === "active" && (
-        <Button variant="secondary-muted" onClick={onRestart}>
-          Restart Test
-          <img src={IconRestart} alt="restart" />
-        </Button>
+        <div className="flex justify-center border-t-1 border-neutral-700">
+          <Button
+            className="mt-400"
+            variant="secondary-muted"
+            onClick={onRestart}
+          >
+            Restart Test
+            <img src={IconRestart} alt="restart" />
+          </Button>
+        </div>
       )}
-    </div>
+    </>
   );
 }
